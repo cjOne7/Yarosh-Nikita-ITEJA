535dcd5fb9c0b0077e3df96fce8e5dddba33e153
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-03-22 17:23:05 +0100

    added Value interface

--- ITEJA_Yarosh/src/main/java/parser/lib/NumberValue.java
+++ ITEJA_Yarosh/src/main/java/parser/lib/NumberValue.java
@@ -1,4 +1,19 @@
 package parser.lib;
 
-public class NumberValue {
+public class NumberValue implements IValue {
+    private final double value;
+
+    public NumberValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public double asDouble() {
+        return value;
+    }
+
+    @Override
+    public String asString() {
+        return Double.toString(value);
+    }
 }
