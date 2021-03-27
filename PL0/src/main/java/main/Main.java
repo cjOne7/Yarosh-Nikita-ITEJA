a59e703d3dbe5fc0d5a2db7cb3b88d4fc48fb0af
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-03-27 17:02:46 +0100

    Update Main.java

--- PL0/src/main/java/main/Main.java
+++ PL0/src/main/java/main/Main.java
@@ -13,8 +13,8 @@ public final class Main {
     public static void main(String[] args) {
         try {
             StringBuilder stringBuilder = new StringBuilder();
-//            Scanner scanner = new Scanner(new File("Program 1.txt"));
-            Scanner scanner = new Scanner(new File("Program 2.txt"));//test values: a = 3, b = -14, c = -5, discriminant = 256, x1 = 5, x2 = -1/3
+            Scanner scanner = new Scanner(new File("Program 1.txt"));
+//            Scanner scanner = new Scanner(new File("Program 2.txt"));//test values: a = 3, b = -14, c = -5, discriminant = 256, x1 = 5, x2 = -1/3
             while (scanner.hasNextLine()) {
                 stringBuilder.append(scanner.nextLine()).append("\n");
             }
@@ -23,14 +23,12 @@ public final class Main {
 
             Lexer lexer = new Lexer();
             List<Token> tokens = lexer.getTokens(stringBuilder.toString());
-//            for (Token token : tokens) {
-//                System.out.println(token);
-//            }
-
-            Parser parser = new Parser(tokens);
-            IStatement program = parser.parseBlock();
-            program.execute();
-
+            for (Token token : tokens) {
+                System.out.println(token);
+            }
+//            Parser parser = new Parser(tokens);
+//            IStatement program = parser.parseBlock();
+//            program.execute();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
