5c79d1fca98af8a81914f0bbdf63febe15aab835
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-03-30 01:59:17 +0200

    Update Main.java

--- ITEJA_Yarosh_v2/src/main/java/main/Main.java
+++ ITEJA_Yarosh_v2/src/main/java/main/Main.java
@@ -4,38 +4,27 @@ import lexer.Lexer;
 import parser.Parser;
 import parser.statements.IStatement;
 import token.Token;
-import token.TokenType;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class Main {
     public static void main(String[] args) {
+        String[] paths = {"Example 1.txt", "Example 2.txt", "Example 3.txt"};
         try {
-            StringBuilder stringBuilder = new StringBuilder();
-            Scanner scanner = new Scanner(new File("Example 1.txt"));
-//            Scanner scanner = new Scanner(new File("Example 2.txt"));
-//            Scanner scanner = new Scanner(new File("Example 3.txt"));
-            while (scanner.hasNextLine()) {
-                stringBuilder.append(scanner.nextLine()).append("\n");
-            }
-            scanner.close();
-            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-
+            String input = new String(Files.readAllBytes(Paths.get(paths[0])), StandardCharsets.UTF_8);
             Lexer lexer = new Lexer();
-            List<Token> tokens = lexer.getTokens(stringBuilder.toString());
+            List<Token> tokens = lexer.getTokens(input);
 //            for (Token token : tokens) {
 //                System.out.println(token);
 //            }
             Parser parser = new Parser(tokens);
             IStatement program = parser.parseBlock();
             program.execute();
-//            System.out.println(program);
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
