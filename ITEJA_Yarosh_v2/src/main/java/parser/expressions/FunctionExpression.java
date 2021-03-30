6573ff62edd96267e9be7a678d0b4fa98a41344d
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-03-30 21:19:47 +0200

    Update FunctionExpression.java

--- ITEJA_Yarosh_v2/src/main/java/parser/expressions/FunctionExpression.java
+++ ITEJA_Yarosh_v2/src/main/java/parser/expressions/FunctionExpression.java
@@ -2,27 +2,39 @@ package parser.expressions;
 
 import parser.lib.Functions;
 import parser.lib.IValue;
+import token.TokenType;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FunctionExpression implements IExpression {
-    private final String function;
+    private final String functionName;
     private final List<IExpression> args;
+    private boolean isExpression;
 
-    public FunctionExpression(String function) {
-        this.function = function;
+    public FunctionExpression(String functionName) {
+        this.functionName = functionName;
         this.args = new ArrayList<>();
     }
 
-    public FunctionExpression(String function, List<IExpression> args) {
-        this.function = function;
+    public FunctionExpression(String functionName, boolean isExpression) {
+        this.functionName = functionName;
+        this.isExpression = isExpression;
+        this.args = new ArrayList<>();
+    }
+
+    public FunctionExpression(String functionName, List<IExpression> args) {
+        this.functionName = functionName;
         this.args = args;
     }
 
     public void addArgument(IExpression argument) {
         args.add(argument);
+    }
+
+    public void addArguments(List<IExpression> args) {
+        this.args.addAll(args);
     }
 
     @Override
@@ -31,11 +43,15 @@ public class FunctionExpression implements IExpression {
         for (int i = 0; i < args.size(); i++) {
             values[i] = args.get(i).eval();
         }
-        return Functions.getFunctionByName(function).execute(values);
+
+        if (Functions.functionHasReturnType(functionName) || (!Functions.functionHasReturnType(functionName) && !isExpression)) {
+            return Functions.getFunctionByName(functionName).execute(values);
+        }
+        throw new RuntimeException("Procedure '" + functionName + "' can't call as expression.");
     }
 
     @Override
     public String toString() {
-        return function + "(" + args.stream().map(expression -> expression.eval().asString()).collect(Collectors.joining(",")) + ")";
+        return functionName + "(" + args.stream().map(expression -> expression.eval().asString()).collect(Collectors.joining(",")) + ")";
     }
 }
