dac66d2863738e61462c06de50de7f12b7d0a705
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-09 02:07:28 +0200

    Update IStatement.java

--- ITEJA_Yarosh_v2/src/main/java/parser/statements/IStatement.java
+++ ITEJA_Yarosh_v2/src/main/java/parser/statements/IStatement.java
@@ -1,7 +1,21 @@
 package parser.statements;
 
+import parser.statements.loops.*;
+import parser.statements.blocks.*;
+import parser.statements.functions.FunctionStatement;
+
 /**
- *
+ * @see BlockStatement
+ * @see StatementBlock
+ * @see FunctionStatement
+ * @see BreakStatement
+ * @see ForStatement
+ * @see WhileStatement
+ * @see RepeatStatement
+ * @see AssignmentStatement
+ * @see ExitStatement
+ * @see IfStatement
+ * @see ReadStatement
  */
 @FunctionalInterface
 public interface IStatement {
